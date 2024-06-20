37ddff2ee03f0123d0ffe2464404f1b6f932e730
Robin Roth <r.roth@mail.de>
2024-06-20 08:52:26 +0200

    Fixed typo in model names

--- frontend/src/@types/conversation.d.ts
+++ frontend/src/@types/conversation.d.ts
@@ -1,5 +1,5 @@
 export type Role = 'system' | 'assistant' | 'user';
-export type Model = 'claude-instant-v1' | 'claude-v2' | "claude-v3-sonnet" | "claude-3-haiku" | "claude-3-opus";
+export type Model = 'claude-instant-v1' | 'claude-v2' | "claude-v3-sonnet" | "claude-v3-haiku" | "claude-v3-opus";
 export type Content = {
   contentType: 'text';
   body: string;
